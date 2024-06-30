2be26a9b4bc62c09bcef1d4daf672c914d684eff
Sebieski <sebuuunio@gmail.com>
2024-06-30 23:55:06 +0200

    Added repositories for Alert and Client

---
+++ AlertManager/AlertManager/Repositories/AlertRepository.cs
@@ -0,0 +1,61 @@
+﻿using AlertManager.DAL;
+using AlertManager.Models;
+using Dapper;
+
+namespace AlertManager.Repositories
+{
+    public class AlertRepository : IRepository<Alert>
+    {
+        private IDapperContext _context;
+
+        public AlertRepository(IDapperContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Alert>> GetAllAsync()
+        {
+            using (var connection = _context.CreateConnection())
+            {
+                return await connection.QueryAsync<Alert>("SELECT * FROM Alerts");
+            }
+        }
+
+        public async Task<int> AddAsync(Alert alert)
+        {
+            var sql = "INSERT INTO Alerts (ClientId, CurrencyPair, Direction, AmountBase, Rate) " +
+                      "VALUES (@ClientId, @CurrencyPair, @Direction, @AmountBase, @Rate); " +
+                      "SELECT CAST(SCOPE_IDENTITY() as int);";
+
+            using (var connection = _context.CreateConnection())
+            {
+                var id = await connection.QuerySingleAsync<int>(sql, alert);
+                return id;
+            }
+        }
+
+        public async Task<Alert> GetByIdAsync(int id)
+        {
+            using (var connection = _context.CreateConnection())
+            {
+                return await connection.QuerySingleOrDefaultAsync<Alert>("SELECT * FROM Alerts WHERE AlertId = @Id", new { id });
+            }
+        }
+
+        public async Task<bool> UpdateAsync(Alert alert)
+        {
+            //Brak potrzeby implementacji - update dokonywany przez usuwanie zbędnych alertów i dodawanie nowych
+            return true;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            using (var connection = _context.CreateConnection())
+            {
+                var affectedRows = await connection.ExecuteAsync("DELETE FROM Alerts WHERE AlertId = @Id", new { id });
+                return affectedRows > 0;
+            }
+        }
+
+    }
+}
---
+++ AlertManager/AlertManager/Repositories/ClientRepository.cs
@@ -0,0 +1,66 @@
+﻿using AlertManager.DAL;
+using AlertManager.Models;
+using Dapper;
+using static Dapper.SqlMapper;
+
+namespace AlertManager.Repositories
+{
+    public class ClientRepository : IRepository<Client>
+    {
+        private IDapperContext _context;
+
+        public ClientRepository(IDapperContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Client>> GetAllAsync()
+        {
+            using (var connection = _context.CreateConnection())
+            {
+                return await connection.QueryAsync<Client>("SELECT * FROM Clients");
+            }
+        }
+
+        public async Task<int> AddAsync(Client client)
+        {
+            var sql = "INSERT INTO Clients (ClientName, CapitalGroup, Exposure, UserId, Alerts) " +
+                      "VALUES (@ClientName, @CapitalGroup, @Exposure, @UserId, @Alerts); " +
+                      "SELECT CAST(SCOPE_IDENTITY() as int);";
+
+            using (var connection = _context.CreateConnection())
+            {
+                var id = await connection.QuerySingleAsync<int>(sql, client);
+                return id;
+            }
+        }
+
+        public async Task<Client> GetByIdAsync(int id)
+        {
+            using (var connection = _context.CreateConnection())
+            {
+                return await connection.QuerySingleOrDefaultAsync<Client>("SELECT * FROM Clients WHERE ClientId = @Id", new { id });
+            }
+        }
+
+        public async Task<bool> UpdateAsync(Client client)
+        {
+            //brak możliwości zmiany nazwy czy grupy kapitałowej klienta, możliwa zmiana przypisanego Dealera lub danych dotyczących ekspozycji na ryzyko walutowe (= zmiana profilu działalności spółki lub sezonowość w biznesie)
+            var sql = "UPDATE Clients SET Exposure = @Exposure, UserId = @UserId WHERE ClientId = @ClientId";
+            using (var connection = _context.CreateConnection())
+            {
+                var affectedRows = await connection.ExecuteAsync(sql, client);
+                return affectedRows > 0;
+            }
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            using (var connection = _context.CreateConnection())
+            {
+                var affectedRows = await connection.ExecuteAsync("DELETE FROM Clients WHERE ClientId = @Id", new { id });
+                return affectedRows > 0;
+            }
+        }
+    }
+}
