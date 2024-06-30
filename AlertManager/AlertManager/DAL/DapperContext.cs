f0d5ecf7f65a2e31cd9f02b0e45fbc226e01ffbe
Sebieski <sebuuunio@gmail.com>
2024-06-30 22:46:00 +0200

    Added DB Connection / Dapper

---
+++ AlertManager/AlertManager/DAL/DapperContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace AlertManager.DAL
+{
+    public class DapperContext : IDapperContext
+    {
+        private readonly IConfiguration _configuration;
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
+        }
+        public IDbConnection CreateConnection() => new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+    }
+}
---
+++ AlertManager/AlertManager/DAL/IDapperContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace AlertManager.DAL
+{
+    public interface IDapperContext
+    {
+        public IDbConnection CreateConnection();
+    }
+}
