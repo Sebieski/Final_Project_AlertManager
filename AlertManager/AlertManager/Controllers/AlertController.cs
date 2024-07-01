17f6e2b7ad4c3a6439abc50d526a5fdca7621e0e
Sebieski <sebuuunio@gmail.com>
2024-07-02 00:02:39 +0200

    Added AlertController and AlertDto

---
+++ AlertManager/AlertManager/Controllers/AlertController.cs
@@ -0,0 +1,84 @@
+﻿using AlertManager.DTO;
+using AlertManager.Models;
+using AlertManager.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AlertManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlertController : ControllerBase
+    {
+        private readonly IRepository<Alert> _repository;
+
+        public AlertController(IRepository<Alert> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAlerts()
+        {
+            var alerts = await _repository.GetAllAsync();
+            return Ok(alerts);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAlertById(int id)
+        {
+            var alert = await _repository.GetByIdAsync(id);
+            if (alert == null)
+            {
+                return NotFound();
+            }
+            return Ok(alert);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAlert([FromBody] AlertDto alert)
+        {
+            var newAlert = new Alert()
+            {
+                ClientId = alert.ClientId,
+                CurrencyPair = alert.CurrencyPair,
+                Direction = alert.Direction,
+                AmountBase = alert.AmountBase,
+                Rate = alert.Rate
+            };
+
+            var createdAlertId = await _repository.AddAsync(newAlert);
+            return CreatedAtAction(nameof(GetAlertById), new { id = createdAlertId }, alert);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAlert(int id, [FromBody] Alert alert)
+        {
+            if (id != alert.AlertId)
+            {
+                return BadRequest();
+            }
+
+            var result = await _repository.UpdateAsync(alert);
+            if (!result)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAlert(int id)
+        {
+            var result = await _repository.DeleteAsync(id);
+            if (!result)
+            {
+                return NotFound();
+            }
+
+            return Ok($"Alert {id} deleted.");
+        }
+    }
+}
---
+++ AlertManager/AlertManager/DTO/AlertDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AlertManager.DTO
+{
+    public class AlertDto
+    {
+        public int ClientId { get; set; }
+        public string CurrencyPair { get; set; }
+        public string Direction { get; set; }
+        public decimal AmountBase { get; set; }
+        public decimal Rate { get; set; }
+    }
+}
