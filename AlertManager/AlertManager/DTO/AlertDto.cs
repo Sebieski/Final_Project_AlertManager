71783c0038da6d0e28ef235b54d6c65ed9e67b40
Sebieski <sebuuunio@gmail.com>
2024-07-03 22:30:10 +0200

    Modfied AlertDto adding validation

--- AlertManager/AlertManager/DTO/AlertDto.cs
+++ AlertManager/AlertManager/DTO/AlertDto.cs
@@ -1,11 +1,27 @@
-﻿namespace AlertManager.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlertManager.DTO
 {
     public class AlertDto
     {
+        [Required]
         public int ClientId { get; set; }
+
+        [Required]
+        [StringLength(7, MinimumLength = 7, ErrorMessage = "Wprowadź ciąg znaków o długości 7.")]
+        [RegularExpression(@"^(EUR\/PLN|USD\/PLN)$", ErrorMessage = "Obsługiwane pary walutowe to: 'EUR/PLN' i 'USD/PLN'.")]
         public string CurrencyPair { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(Buy|Sell)$", ErrorMessage = "Dopuszczalne kierunki: 'Buy' lub 'Sell'.")]
         public string Direction { get; set; }
+
+        [Required]
+        [Range(0, 50000000)]
         public decimal AmountBase { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d+\.\d{4}$", ErrorMessage = "Format kursu X.XXXX.")]
         public decimal Rate { get; set; }
     }
 }
