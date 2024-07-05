ba82255e070c93af8917ddab313d508c0abe9aba
Sebieski <sebuuunio@gmail.com>
2024-07-05 22:06:02 +0200

    Updated file structure web-app

---
+++ AlertManager/alert-manager-web/src/components/FXRates.jsx
@@ -0,0 +1,49 @@
+import {useEffect, useState} from "react";
+
+const FXRates = () => {
+    const [currentRates, setCurrentRates] = useState(null);
+    const API = "http://api.nbp.pl/api/exchangerates/tables/A?format=json";
+
+    useEffect(() => {
+        fetch(API)
+            .then(response => response.json())
+            .then(data => {
+                setCurrentRates(data[0]);
+            })
+            .catch(err => {
+            console.error("Błąd pobierania danych:", err);
+        });
+    }, []);
+
+    if (currentRates === null) {
+        return <h3>Pobieram aktualne kursy...</h3>
+    } else {
+        return (
+            <div>
+                <div className="container mt-5 pt-5">
+                    <h3 className="mb-4 text-center">Kursy aktualne na {currentRates.effectiveDate}</h3>
+                    <table className="table table-striped table-dark table-hover">
+                        <thead>
+                        <tr>
+                            <th scope="col">Waluta</th>
+                            <th scope="col">Kod</th>
+                            <th scope="col">Aktualny kurs</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {currentRates.rates.map((rate) => (
+                            <tr key={rate.code}>
+                                <td>{rate.currency}</td>
+                                <td>{rate.code}/PLN</td>
+                                <td>{rate.mid}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default FXRates;
