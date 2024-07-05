40044322e555d6137560f9585cf3f8b13c528a47
Sebieski <sebuuunio@gmail.com>
2024-07-05 21:16:43 +0200

    Added Footer component to web-app

---
+++ AlertManager/alert-manager-web/src/components/common/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="row">
+                <div className="col-md-12">
+                    <p>&copy; 2024 by <a href="mailto:sebuuunio@gmail.com">Sebieski</a>. Projekt końcowy kursu .NET Developer
+                        by CodersLab.
+                        Wszelkie prawa zastrzeżone.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Footer;
