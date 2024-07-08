3e9acc17feb5574cae7b752006d5cfbc1a349231
Sebieski <sebuuunio@gmail.com>
2024-07-08 17:49:01 +0200

    Updated Header with logout funcionality in web-app

--- AlertManager/alert-manager-web/src/components/common/Header.jsx
+++ AlertManager/alert-manager-web/src/components/common/Header.jsx
@@ -1,8 +1,8 @@
 import {NavLink} from "react-router-dom";
-import './Header.css';
+import '../../styles/Header.css';
+import PropTypes from "prop-types";
 
-
-const Header = () => {
+const Header = ({setToken}) => {
     const activeStyle = {
         backgroundColor: 'limegreen',
         border: '2px solid #0d4024',
@@ -13,19 +13,27 @@ const Header = () => {
         transition: 'background-color 0.3s ease, color 0.3s ease'
     };
 
+    const handleLogout = () => {
+        setToken(null);
+    }
+
     return (
         <nav>
-            <h1>Witaj w Alert Manager v. 1.0!</h1>
+            <h1>Alert Manager v. 1.0!</h1>
             <ul>
                 <li><NavLink to="/" style={({ isActive }) => isActive ? activeStyle : undefined}>Strona główna</NavLink></li>
                 <li><NavLink to="/portfolio" style={({ isActive }) => isActive ? activeStyle : undefined}>Twoi klienci</NavLink></li>
                 <li><NavLink to="/fxrates" style={({ isActive }) => isActive ? activeStyle : undefined}>Aktualne kursy</NavLink></li>
                 <li><NavLink to="/alerts" style={({ isActive }) => isActive ? activeStyle : undefined}>Aktywne alerty</NavLink></li>
                 <li><NavLink to="/addalert" style={({ isActive }) => isActive ? activeStyle : undefined}>Dodaj alert</NavLink></li>
-                <li><NavLink to="/logout" style={({ isActive }) => isActive ? activeStyle : undefined}>Wyloguj</NavLink></li>
+                <li><button onClick={handleLogout}>Wyloguj</button></li>
             </ul>
         </nav>
     )
 }
 
 export default Header;
+
+Header.propTypes = {
+    setToken: PropTypes.func,
+}
