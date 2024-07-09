6361bb7d7fbe27ddbdee42020f6a702e7207c0c7
Sebieski <sebuuunio@gmail.com>
2024-07-09 21:50:51 +0200

    Added App component with styles in web-app

---
+++ AlertManager/alert-manager-web/src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css';
+import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
+import Header from "./components/common/Header.jsx";
+import FXRates from "./components/FXRates.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from "./components/common/Footer.jsx";
+import Portfolio from "./components/Portfolio.jsx";
+import {useState} from "react";
+import LoginComponent from "./components/LoginComponent.jsx";
+import Dashboard from "./components/Dashboard.jsx";
+import ActiveAlerts from "./components/ActiveAlerts.jsx";
+
+function App() {
+    const [userId, setUserId] = useState(null);
+    const [token, setToken] = useState(null);
+    console.log(token);
+
+  return (
+    <>
+        <BrowserRouter>
+            {token && <Header setToken={setToken}/>}
+            <Routes>
+                <Route exact path="/" element={token? <Dashboard token={token} userId={userId} /> : <LoginComponent setUserId={setUserId} setToken={setToken} />} />
+                <Route path="/portfolio" element={token? <Portfolio token = {token} userId = {userId} /> : <Navigate to="/" />} />
+                <Route path="/fxrates" element={token ? <FXRates /> : <Navigate to="/" />} />
+                <Route path="/alerts" element={token ? <ActiveAlerts token={token} userId={userId}/> : <Navigate to="/" />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    </>
+  )
+}
+
+export default App
